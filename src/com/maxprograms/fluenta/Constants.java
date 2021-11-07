3f069c7ce0434989b84913bf9ac2dcf1d908fe87
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-11-07 09:36:41 -0300

    Updated for release 2.2.0

--- src/com/maxprograms/fluenta/Constants.java
+++ src/com/maxprograms/fluenta/Constants.java
@@ -19,8 +19,8 @@ public class Constants {
 	}
 	
 	public static final String NAME = "Fluenta"; //$NON-NLS-1$
-	public static final String VERSION = "2.1.0"; //$NON-NLS-1$
-	public static final String BUILD = "202100827_1131"; //$NON-NLS-1$
+	public static final String VERSION = "2.2.0"; //$NON-NLS-1$
+	public static final String BUILD = "20211107_0808"; //$NON-NLS-1$
 	
 	public static final String SUCCESS = "0"; //$NON-NLS-1$
 }
