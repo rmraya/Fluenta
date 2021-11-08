c765c6ceac502007d118b74752c8072b9646c8ef
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2021-11-08 08:40:14 -0300

    Updated build date

--- src/com/maxprograms/fluenta/Constants.java
+++ src/com/maxprograms/fluenta/Constants.java
@@ -20,7 +20,7 @@ public class Constants {
 	
 	public static final String NAME = "Fluenta"; //$NON-NLS-1$
 	public static final String VERSION = "2.2.0"; //$NON-NLS-1$
-	public static final String BUILD = "20211107_0808"; //$NON-NLS-1$
+	public static final String BUILD = "20211108_0754"; //$NON-NLS-1$
 	
 	public static final String SUCCESS = "0"; //$NON-NLS-1$
 }
