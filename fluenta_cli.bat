d92ca999ae4fe406dd4f3f78951999fe11d7a95a
Rodolfo M. Raya <rmraya@users.noreply.github.com>
2022-11-24 11:57:02 -0300

    updated cli

--- fluenta_cli.bat
+++ fluenta_cli.bat
@@ -1,2 +1,3 @@
 @echo off
-bin\java -cp "lib/h2-1.4.200.jar" --module-path lib -m fluenta/com.maxprograms.fluenta.CLI %1 %2 %3 %4 %5 %6 %7 %8 %9
+pushd "%~dp0"
+bin\java -cp "lib/h2-1.4.200.jar" --module-path lib -m fluenta/com.maxprograms.fluenta.CLI %*
