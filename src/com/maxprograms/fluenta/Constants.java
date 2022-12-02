7feee4dcaf622ed32a221a85897ca465f69005a9
Rodolfo M. Raya <rmraya@maxprograms.com>
2022-12-02 14:34:46 -0300

    Improved support for DITA from Astoria CMS

--- src/com/maxprograms/fluenta/Constants.java
+++ src/com/maxprograms/fluenta/Constants.java
@@ -19,6 +19,6 @@ public class Constants {
 	}
 	
 	public static final String NAME = "Fluenta"; 
-	public static final String VERSION = "3.0.0"; 
-	public static final String BUILD = "20221124_1111"; 
+	public static final String VERSION = "3.1.0"; 
+	public static final String BUILD = "20221202_1132"; 
 }
